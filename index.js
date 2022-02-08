c7339f04ab18d15556e75d1e05f6d0dd3999b72c
Gatesvert81 <mathiasmartey81@outlook.com>
2022-02-08 21:50:31 +0000

    make it better

---
+++ index.js
@@ -0,0 +1,47 @@
+//Dependencies: 
+//yarn add express cors twilio 
+
+const express = require('express'); 
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const twilio = require('twilio'); 
+
+const port = process.env.PORT || 4000
+
+//twilio requirements -- Texting API 
+const accountSid = 'AC8c104137655941a6a4e522c192c7c6b0';
+const authToken = 'e1c0d119e42193d7fd1cbb70a4ddc47e'; 
+const client = new twilio(accountSid, authToken);
+
+const app = express(); //alias
+
+app.use(bodyParser.json())
+app.use(cors()); //Blocks browser from restricting any data
+
+//Welcome Page for the Server 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express Server')
+})
+
+//Twilio 
+app.post('/send-text', (req, res) => {
+    //Welcome Message
+    res.send('Hello to the Twilio Server')
+
+    //_GET Variables
+    const { messageText, to } = req.body;
+
+    console.log({messageText})
+
+
+    // Send Text
+    client.messages.create({
+        body: messageText,
+        to: to,  // Text this number 
+        from: '8558950429' // From a valid Twilio number
+    })
+    .then((message) => console.log(message.body))
+    .catch((error) => console.log(error));
+})
+
+app.listen(port, () => console.log("Running on Port 4000"))
